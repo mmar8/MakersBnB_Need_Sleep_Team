5694d7007ca0b1bc3140ecefe675208ec87b77ac
Simon <01secn@gmail.com>
2019-03-11 15:17:53 +0000

    Space creates in database

--- Gemfile
+++ Gemfile
@@ -3,6 +3,7 @@ source 'https://rubygems.org'
 ruby "2.6.0"
 gem "pg"
 gem "sinatra"
+gem 'active_record'
 
 group :test do
   gem "capybara"
--- app.rb
+++ app.rb
@@ -1,6 +1,19 @@
 require "sinatra/base"
 require "pg"
+require './lib/space.rb'
 
 class SleeperManager<Sinatra::Base
-    run! if app_file == $0
+  get '/space/creation' do
+    erb :create_space
+  end
+
+  post '/space' do
+    Space.create(
+      name: params[:name], 
+      description: params[:description],
+      price: params[:price]
+    )
+  end
+
+  run! if app_file == $0
 end
