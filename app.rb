3c60e0765be60ec4aaeb94faf5b34e14e7015efe
Soroush Khosravi <sorush.kh68@gmail.com>
2019-03-11 15:12:18 +0000

    setup welcome pages - name/email etc

--- app.rb
+++ app.rb
@@ -2,5 +2,24 @@ require "sinatra/base"
 require "pg"
 
 class SleeperManager<Sinatra::Base
+  enable :sessions
+
+  get '/' do
+    erb :index
+  end
+
+  get '/signup' do
+    erb :signup
+  end
+
+  post '/signup' do
+    session[:name] = params[:name]
+    redirect '/welcome'
+  end
+
+  get '/welcome' do
+    @name = session[:name]
+    erb :welcome
+  end
     run! if app_file == $0
 end
---
+++ spec/features/sign_up_spec.rb
@@ -0,0 +1,15 @@
+require "pg"
+
+feature 'Sign up to Sleeper' do
+  scenario 'User signs up to Sleeper' do
+    visit '/'
+    click_button('sign-up')
+    expect(current_path).to eq '/signup'
+    fill_in('name', with: 'Bob')
+    fill_in('username', with: 'BobBuilder')
+    fill_in('password', with: '1234')
+    fill_in('email', with: 'Bob@bob.bob')
+    click_button('submit')
+    expect(page).to have_content('Welcome Bob')
+  end
+end
