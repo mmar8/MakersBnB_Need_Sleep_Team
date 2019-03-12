e7293b22f43257dc3ccfbbc537765202f6c31e52
Soroush Khosravi <sorush.kh68@gmail.com>
2019-03-12 11:24:23 +0000

    ActiveRecord can be used nowgit add .git add .

--- spec/setup_test_helper.rb
+++ spec/setup_test_helper.rb
@@ -1,9 +1,4 @@
-require 'pg'
+require "./lib/user.rb"
 def setup_test_database
-p "Setting up test database..."
-
-connection = PG.connect(dbname: 'sleepers_test')
-
-# Clear the users table
-connection.exec("TRUNCATE users;")
+  User.delete_all()
 end
