6e53f6c1ecb05db97a79fea35a3224cc5135e860
Simon <01secn@gmail.com>
2019-03-12 11:40:38 +0000

    Moved space routes into separate space controller to meet SRP

--- app.rb
+++ app.rb
@@ -3,28 +3,10 @@ require "pg"
 require './lib/space.rb'
 
 class SleeperManager<Sinatra::Base
-  get '/space/creation' do
-    erb :create_space
+  configure do
+    set :views, 'views/'
+    set :root, File.expand_path('../../../', __FILE__)
   end
-
-  post '/space' do
-    Space.create(
-      name: params[:name], 
-      description: params[:description],
-      price: params[:price]
-    )
-    redirect('/space-created')
-  end
-
-  get '/space-created' do 
-    erb :space_created
-  end
-
-  get '/spaces' do
-    @spaces = Space.all
-    
-    erb :spaces
-  end
-
+  
   run! if app_file == $0
 end
--- config.ru
+++ config.ru
@@ -1,5 +1,7 @@
 require_relative "./app.rb"
+require_relative "./controllers/space_controller.rb"
 
 ActiveRecord::Base.establish_connection(ENV['SLEEPERS_DB_URL'])
 
+use SpaceController
 run SleeperManager
