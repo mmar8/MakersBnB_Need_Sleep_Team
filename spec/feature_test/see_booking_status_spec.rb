a0697d7997ea577d2133e15cf1c620809bd4b1af
Simon <01secn@gmail.com>
2019-03-15 11:43:56 +0000

    Refactored view booking status tests by loading required test data into db, shortening tests, over using frontend to create data

--- spec/feature_test/see_booking_status_spec.rb
+++ spec/feature_test/see_booking_status_spec.rb
@@ -1,24 +1,14 @@
 feature "We can see the status of the bookings we have made" do
-  it "shows the status of the bookings we have made" do
+  scenario "shows the status of the bookings we have made" do
     owner = create_user_in_db('Jane')
-    space = Space.create(
-      name: "London house",
-      description: "beautiful mansion",
-      owner_id: owner.id,
-      price: "100",
-    )
-    guest = create_user_then_login('Sam')
-    fill_in "request_description", with: "I want to rent your house."
-    click_button "Send"
-    click_button "Back to spaces"
-    click_button "MyBookings"
-    expect(page).to have_content("I want to rent your house.")
-    expect(page).to have_content("pending")
-    visit "/spaces"
-    click_button "log-out"
-    guest_2 = create_user_then_login('soroush')
-    create_generic_bookings(guest_2.id)
+    create_generic_bookings(owner.id)
+
+    owner2 = create_user_in_db('Simon')
+    guest = create_generic_bookings(owner2.id)[:guest]
+
+    login(username: guest.username, password: guest.password)
     visit "/requested_bookings"
+
     bookings = page.all(".booking")
     expect(bookings.length).to eq 2
   end
