39c57566f07722fd3ec7b66c4ac9eb88ff67812a
Soroush Khosravi <sorush.kh68@gmail.com>
2019-03-15 11:28:30 +0000

    some changes need to be done.

--- spec/feature_test/see_booking_status_spec.rb
+++ spec/feature_test/see_booking_status_spec.rb
@@ -17,11 +17,9 @@ feature "We can see the status of the bookings we have made" do
     visit "/spaces"
     click_button "log-out"
     guest_2 = create_user_then_login('soroush')
-    fill_in "request_description", with: "I want your house"
-    click_button "Send"
-    click_button "Back to spaces"
-    click_button "MyBookings"
-    expect(page).to have_content("I want your house")
-    expect(page).not_to have_content("I want to rent your house.")
+    create_generic_bookings(guest_2.id)
+    visit "/requested_bookings"
+    bookings = page.all(".booking")
+    expect(bookings.length).to eq 2
   end
 end
